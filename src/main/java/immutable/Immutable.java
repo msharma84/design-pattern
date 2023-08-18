9acc6076571d3ea0b9977182c1d829dd36cb14ba
Mohit Sharma <mohitsharma603@gmail.com>
2023-08-18 17:52:04 +0530

    Immutable Design Pattern

---
+++ src/main/java/immutable/Immutable.java
@@ -0,0 +1,17 @@
+package designpattern.immutable;
+
+public final class Immutable {
+	
+	final String value;
+
+	public Immutable(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+}
+
+
---
+++ src/main/java/immutable/ImmutableDemo.java
@@ -0,0 +1,13 @@
+package designpattern.immutable;
+
+public class ImmutableDemo {
+
+	public static void main(String[] args) {
+		
+		Immutable im = new Immutable("A");
+		
+		System.out.println("Immutable Value ->"+im.getValue());
+		
+	}
+
+}
